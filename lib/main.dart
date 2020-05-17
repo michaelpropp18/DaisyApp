a9067061b48b402cebe80bbb25174cf5b027aa2e
Michael Propp <michaelpropp8664@gmail.com>
2020-05-17 11:01:58 -0400

    Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:daisy_app/screens/settings_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import './screens/summary_screen.dart';
+import './screens/add_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +13,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return CupertinoApp(
       home: HomeScreen(),
     );
@@ -37,9 +42,30 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       tabBuilder: (context, index) {
-        return CupertinoTabView(builder: (_) {
-          return SummaryScreen();
-        });
+        CupertinoTabView returnValue;
+        switch (index) {
+          case 0:
+            returnValue = CupertinoTabView(builder: (_) {
+              return SummaryScreen();
+            });
+            break;
+          case 1:
+            returnValue = CupertinoTabView(builder: (_) {
+              return AddScreen();
+            });
+            break;
+          case 2:
+            returnValue = CupertinoTabView(builder: (_) {
+              return SettingsScreen();
+            });
+            break;
+          default:
+            returnValue = CupertinoTabView(builder: (_) {
+              return SummaryScreen();
+            });
+            break;
+        }
+        return returnValue;
       },
     );
   }
---
+++ lib/screens/add_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Center(child: Text('This worked')),
+    );
+  }
+}
