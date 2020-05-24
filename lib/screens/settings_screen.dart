fdb7506426d7d53857c579377fd71a28b0e90520
Michael Propp <michaelpropp8664@gmail.com>
2020-05-24 15:36:12 -0400

    Settings

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -20,80 +20,81 @@ class _SettingsScreenState extends State<SettingsScreen> {
             style: TextStyle(color: Colors.white),
           ),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('PROFILE'),
-            Text('Name'),
-            CupertinoTextField(
-              placeholder: "Michael",
-            ),
-            Text('Phone Number'),
-            CupertinoTextField(
-              placeholder: "404-858-2246",
-            ),
-            Row(
-              children: <Widget>[
-                Text('Color'),
-                Container(
-                  width: 18,
-                  height: 18,
-                  decoration: new BoxDecoration(
-                    color: Colors.green,
-                    borderRadius:
-                        new BorderRadius.all(new Radius.circular(50.0)),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('PROFILE'),
+              ),
+              Text('Phone Number'),
+              CupertinoTextField(
+                placeholder: "404-858-2246",
+              ),
+              Row(
+                children: <Widget>[
+                  Text('Color'),
+                  Container(
+                    width: 18,
+                    height: 18,
+                    decoration: new BoxDecoration(
+                      color: Colors.green,
+                      borderRadius:
+                          new BorderRadius.all(new Radius.circular(50.0)),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Text(''),
-            Text('NOTIFICATIONS'),
-            Row(
-              children: <Widget>[
-                Text('Push: '),
-                Container(
-                  child: CupertinoSwitch(
-                    onChanged: (bool value) {
-                      setState(() => pushNotifications = value);
-                    },
-                    value: pushNotifications,
+                ],
+              ),
+              Text(''),
+              Text('NOTIFICATIONS'),
+              Row(
+                children: <Widget>[
+                  Text('Push: '),
+                  Container(
+                    child: CupertinoSwitch(
+                      onChanged: (bool value) {
+                        setState(() => pushNotifications = value);
+                      },
+                      value: pushNotifications,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Text('Text sms: '),
+                  Container(
+                    child: CupertinoSwitch(
+                      onChanged: (bool value) {
+                        setState(() => textNotifications = value);
+                      },
+                      value: textNotifications,
+                    ),
+                  )
+                ],
+              ),
+              Text(''),
+              Text('OTHER MEMBERS:'),
+              Row(
+                children: <Widget>[
+                  Text('Michael'),
+                  Container(
+                    width: 18,
+                    height: 18,
+                    decoration: new BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius:
+                          new BorderRadius.all(new Radius.circular(50.0)),
+                    ),
                   ),
-                )
-              ],
-            ),
-             Row(
-              children: <Widget>[
-                Text('Text sms: '),
-                Container(
-                  child: CupertinoSwitch(
-                    onChanged: (bool value) {
-                      setState(() => textNotifications = value);
-                    },
-                    value: textNotifications,
-                  ),
-                )
-              ],
-            ),
-            Text(''),
-            Text('OTHER MEMBERS:'),
-            Row(
-              children: <Widget>[
-                Text('Michael'),
-                Container(
-                  width: 18,
-                  height: 18,
-                  decoration: new BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius:
-                        new BorderRadius.all(new Radius.circular(50.0)),
-                  ),
-                ),
-              ],
-            ),
-            Text('Megan'),
-            Text('Matthew'),
-            Text('Susan'),
-          ],
+                ],
+              ),
+              Text('Megan'),
+              Text('Matthew'),
+              Text('Susan'),
+            ],
+          ),
         ));
   }
 }
